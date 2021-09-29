c4fdc4aecde074cff8666ac4eb43deeacc829da9
Diego Garces Jimenez <diegogarcesj1@gmail.com>
2021-09-28 21:44:13 -0300

    Added fake record

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Categories
+10.times { Category.create!(name: Faker::Lorem.word) }
+
+# Create sources
+10.times { Source.create!(name: Faker::Company.name) }
+
+# Create Articles
+50.times {
+  category = Category.all.sample
+  source = Source.all.sample
+  Article.create!(
+    title: Faker::Lorem.sentence,
+    url: Faker::Internet.url,
+    category: category,
+    source: source
+  )
+}
